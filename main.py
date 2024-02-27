bd789035eab9637aafe49cbe073e93bf0c54f192
Oleg <oleg@massdriver95.ru>
2024-02-27 12:30:16 +0300

    added existing and new product handling

--- main.py
+++ main.py
@@ -8,32 +8,31 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 #appdatabase = 1 #get from JSON 
-with open('project_db/data/products.json') as json_data:
+datafile = 'project_db/data/products.json'
+with open(datafile) as json_data:
 	appdatabase = json.load(json_data)
 pic = 'images/barcode.png'
 
-def get_image(file):
+def get_image(file_path:str):
 	"""Проверяет файл на пригодность к обработке. Получает файл, возвращает изображение или инструкцию
 	"""
 	#file proof to image
 	#note = 'F* u upload a photo'
 	#return image if proof else print(note)
-	image_path = file
-	image = Image.open(image_path)
+	image = Image.open(file_path)
 	return image
 
-def format_image():
+def format_image(raw_img):
 	"""Форматирует фотографию до пригодной картинки с баркодом. Возвращает пригодную картинку.
 	"""
-	#find barcode
-	#crop it
-	#return image with barcode
+	#find and mask the barcode on image
+	#image_with_barcode = crop(mask(raw_img))
+	#return image_with_ barcode
 	pass
 
-def scan(barcode_image):
+def decode(barcode_image):
 	"""Обрабатывает пригодную картинку. Возвращает стринг с кодом
 	"""
-
 	#barcode_num = '8887290146203'
 	barcode_num = decode(barcode_image)
 	return barcode_num
@@ -45,23 +44,48 @@ def barcode_corrector(barcode_raw):
 	#return barcode_num
 	pass
 
-def known_product(barcode_num):
-	"""Проверяет наличие товара в базе данных сервиса. Переключается в дальнейшие процессы.
-	"""
-	if barcode_num in appdatabase:
-		pass #proceed to script to show product
-	else:
-		pass #proceed to script to add product and review
-
 def scanner():
 	"""Включает сканер, обрабатывает изображение. Возвращает стринг с кодом
 	"""
 	#format_image()
-	barcode_num = scan(get_image(pic))
+	barcode_num = decode(get_image(pic))
 	for obj in barcode_num:
 		data = obj.data
 	return data
 
+def get_known_product(barcode:str, datasheet:list):
+	"""Обращается к базе данных в поисках товара. Возвращает массив информации о конкретном товаре
+	"""
+	# call database searching for barcode
+	# get product by barcode
+	product = list(filter(lambda x:x["barcode"]==barcode,datasheet))
+	return product
+
+def known_product(barcode, datasheet):
+	"""Проверяет наличие товара в базе данных сервиса. Переключается в дальнейшие процессы.
+	"""
+	if barcode in datasheet:
+		product = get_known_product(barcode, datasheet)
+	else:
+		
+		#proceed to script to add product and review
+		pass
+
+def input_new_data():
+	"""Помогает в создании новой записи в БД в формате массива. Возвращает массив с информацией к записи.
+	"""
+	#new_data =[]
+	#return new_data
+	pass
+
+def add_new(data:dict, datafile):
+	"""Вносит новую запись в базу данных.
+	"""
+	#with open(datafile, 'w') as outfile:
+			#json.dump(data, outfile)
+	success = False
+	#return success
+	pass
 
 def main():
 		print(scanner())
